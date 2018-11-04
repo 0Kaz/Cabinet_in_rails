66e382b98c3a782b86969a9fa5cbfbdaab7ccf37
Omar Kaz <kazeb.omar@gmail.com>
2018-11-04 18:48:15 +0100

    Layout done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :docs
+  authenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
+
+
 end
